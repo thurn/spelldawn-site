5275c3d324d856168dc7d085c04be92ceac4dd6d
Derek Thurn <derek@fake.email>
2022-07-30 13:46:35 -0700

    table of contents

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,42 @@
-import {SpelldawnCard} from './card';
+import { SpelldawnCard } from './card';
 
 export {SpelldawnCard};
+
+const anchors = (window as any).anchors;
+anchors.add('h1, h2, h3');
+generateTableOfContents(anchors.elements);
+
+
+// Script from https://www.roberthorvick.com/blog/dynamically-creating-table-of-contents-from-anchorjs-elements
+function generateTableOfContents(els: Array<Element>) {
+  var anchoredElText,
+    anchoredElHref,
+    ul = document.createElement('UL');
+
+  document.getElementById('table-of-contents')!.appendChild(ul);
+
+  for (var i = 0; i < els.length; i++) {
+    anchoredElText = els[i].textContent;
+    anchoredElHref = els[i]
+      .querySelector('.anchorjs-link')!
+      .getAttribute('href');
+    addNavItem(
+      ul,
+      anchoredElHref!,
+      anchoredElText!,
+      'toc-li-'.concat(els[i].tagName)
+    );
+  }
+}
+
+function addNavItem(ul: Element, href: string, text: string, listClass: string) {
+  var listItem = document.createElement('LI'),
+    anchorItem = document.createElement('A') as HTMLAnchorElement,
+    textNode = document.createTextNode(text);
+  listItem.className = listClass;
+
+  anchorItem.href = href;
+  ul.appendChild(listItem);
+  listItem.appendChild(anchorItem);
+  anchorItem.appendChild(textNode);
+}
